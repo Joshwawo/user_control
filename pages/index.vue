13893bda24f1a79a563ddf9930ffe483e9406378
Joshwawo <masacota_masacota@hotmail.com>
2023-07-17 19:17:33 -0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+const {data} =  await useFetch('/api/hello')
+
+console.log(data.value)
+
+</script>
+
+
+<template>
+    <div class="bg-gray-700 h-screen text-gray-300">
+        <p>Hola desde el inicio</p>
+    </div>
+</template>
